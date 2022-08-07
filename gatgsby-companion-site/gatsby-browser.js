889cb3922e80035ef599f5d0ff7429f2b8b38173
LayersOfAbstraction <jnash51@live.com>
2022-08-07 22:52:09 +1000

    delete abstracted logic

--- gatgsby-companion-site/gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-import "../gatgsby-companion-site/src/assets/css/font-awesome.min.css"
-import "../gatgsby-companion-site/src/assets/css/main.css"
-import "../gatgsby-companion-site/src/assets/css/noscript.css"
-
