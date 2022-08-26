1e47db22758d7460a44188e004086d319ca0b4d8
LayersOfAbstraction <jnash51@live.com>
2022-08-26 21:32:28 +1000

    abstract sidebar successfully into seprate component

--- gatgsby-companion-site/src/components/sidebar.js
+++ gatgsby-companion-site/src/components/sidebar.js
@@ -1,20 +1,116 @@
-import * as React from 'react'
-export const Foo =  () => (
-    <>
-      <div id="mySidebar" className="sidebar">
-        <a href="javascript:void(0)" className="closebtn" onclick="closeNav()"></a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Clients</a>
-        <a href="#">Contact</a>
-      </div>
-      <div id="main">
-        <button className="openbtn" onclick="openNav()">
-          ☰ Open Sidebar
-        </button>
-        <h2>Collapsed Sidebar</h2>
-        <p>Content...</p>
-      </div>
+import styled, {createGlobalStyle} from "styled-components"
+import React, { useState} from "react"
+import { Link } from "gatsby"
+
+
+const Global = createGlobalStyle`
+  body, html{
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+  }
+`
+const MenuIcon = styled.button`
+  position: fixed;
+  top: 2rem;
+  right: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 1.5rem;
+  height: 1.5rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  z-index: 999;
+
+  div {
+    width: 1.5rem;
+    height: 0.2rem;
+    background: white;
+    border-radius: 5px;
+    transform-origin: 1px;
+    position: relative;
+    transition: opacity 300ms, transform 300ms;
+
+    :first-child{
+      transform: ${({nav}) => (nav ? 'rotate(45deg)': 'rotate(0)')}
+    } 
+    
+
+    :nth-child(2){
+      opacity: ${({nav}) => (nav ? "0" : "1")}
+    } 
+    
+    :nth-child(3){
+      transform: ${({nav}) => (nav ? 'rotate(-45deg)': 'rotate(0)')}
+    }
+  }  
+`
+
+const Menulinks = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
+  width: 25%;
+  position: fixed;
+  transition: 300ms;
+  background-color: black;
+  top: 0;
+  right: 0;
+  z-index: 998;
+  transform: ${({ nav }) => (nav ? "translateX(0)" : "translateX(100%)")};
+
+  ul{
+    list-style-type: none;
+  }
+
+  li{
+    margin-top: 1rem
+  }
+
+
+   a{
+     text-decoration: none; 
+     color: white;
+     font-size: 1.5rem;
+     transition: color 300ms;
+   }
+
+  :hover {
+    color: #6ab4ff    
+  }
+`
+
+export const Sidebar =  () => {
+  const [nav, showNav] = useState(false);
+return(
+<>
+<Global />
+          <MenuIcon nav={nav} onClick={() => showNav(!nav)}>
+            <div/>
+            <div/>
+            <div/>
+          </MenuIcon>
+          <Menulinks nav={nav}>
+            <ul>
+              <Link to="#">Blog</Link>
+            </ul>
+            <ul>
+              <Link to="#">home</Link>
+            </ul>
+            <h3>Social</h3>
+            <ul className="icons alt">
+              <li><Link to="#" class="icon alt fa-twitter"><span class="label">Twitter</span></Link></li>
+              <li><Link to="#" class="icon alt fa-facebook"><span class="label">Facebook</span></Link></li>
+              <li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+              <li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+            </ul>
+          </Menulinks>
     </>
-  )
-  
+  );
+}
+
+
--- gatgsby-companion-site/src/pages/index.js
+++ gatgsby-companion-site/src/pages/index.js
@@ -1,97 +1,97 @@
 import { Link } from "gatsby"
-import styled, {createGlobalStyle} from "styled-components"
 import React, { useState} from "react"
 import pic1 from "../images/pic01.jpg"
 import pic2 from "../images/pic02.jpg"
 import pic3 from "../images/pic03.jpg"
+import { Sidebar } from "../components/sidebar"
 
 import "../assets/css/font-awesome.min.css"
 import "../assets/css/main.css"
 import "../assets/css/noscript.css"
 
-const Global = createGlobalStyle`
-  body, html{
-    margin: 0;
-    padding: 0;
-    overflow-x: hidden;
-  }
-`
-const MenuIcon = styled.button`
-  position: fixed;
-  top: 2rem;
-  right: 2rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  width: 1.5rem;
-  height: 1.5rem;
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  z-index: 999;
+// const Global = createGlobalStyle`
+//   body, html{
+//     margin: 0;
+//     padding: 0;
+//     overflow-x: hidden;
+//   }
+// `
+// const MenuIcon = styled.button`
+//   position: fixed;
+//   top: 2rem;
+//   right: 2rem;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+//   width: 1.5rem;
+//   height: 1.5rem;
+//   background: transparent;
+//   border: none;
+//   cursor: pointer;
+//   z-index: 999;
 
-  div {
-    width: 1.5rem;
-    height: 0.2rem;
-    background: white;
-    border-radius: 5px;
-    transform-origin: 1px;
-    position: relative;
-    transition: opacity 300ms, transform 300ms;
+//   div {
+//     width: 1.5rem;
+//     height: 0.2rem;
+//     background: white;
+//     border-radius: 5px;
+//     transform-origin: 1px;
+//     position: relative;
+//     transition: opacity 300ms, transform 300ms;
 
-    :first-child{
-      transform: ${({nav}) => (nav ? 'rotate(45deg)': 'rotate(0)')}
-    } 
+//     :first-child{
+//       transform: ${({nav}) => (nav ? 'rotate(45deg)': 'rotate(0)')}
+//     } 
     
 
-    :nth-child(2){
-      opacity: ${({nav}) => (nav ? "0" : "1")}
-    } 
+//     :nth-child(2){
+//       opacity: ${({nav}) => (nav ? "0" : "1")}
+//     } 
     
-    :nth-child(3){
-      transform: ${({nav}) => (nav ? 'rotate(-45deg)': 'rotate(0)')}
-    }
-  }  
-`
+//     :nth-child(3){
+//       transform: ${({nav}) => (nav ? 'rotate(-45deg)': 'rotate(0)')}
+//     }
+//   }  
+// `
 
-const Menulinks = styled.nav`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  height: 100vh;
-  width: 25%;
-  position: fixed;
-  transition: 300ms;
-  background-color: black;
-  top: 0;
-  right: 0;
-  z-index: 998;
-  transform: ${({ nav }) => (nav ? "translateX(0)" : "translateX(100%)")};
+// const Menulinks = styled.nav`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   text-align: center;
+//   height: 100vh;
+//   width: 25%;
+//   position: fixed;
+//   transition: 300ms;
+//   background-color: black;
+//   top: 0;
+//   right: 0;
+//   z-index: 998;
+//   transform: ${({ nav }) => (nav ? "translateX(0)" : "translateX(100%)")};
 
-  ul{
-    list-style-type: none;
-  }
+//   ul{
+//     list-style-type: none;
+//   }
 
-  li{
-    margin-top: 1rem
-  }
+//   li{
+//     margin-top: 1rem
+//   }
 
 
-   a{
-     text-decoration: none; 
-     color: white;
-     font-size: 1.5rem;
-     transition: color 300ms;
-   }
+//    a{
+//      text-decoration: none; 
+//      color: white;
+//      font-size: 1.5rem;
+//      transition: color 300ms;
+//    }
 
-  :hover {
-    color: #6ab4ff    
-  }
-`
+//   :hover {
+//     color: #6ab4ff    
+//   }
+// `
 
 const Home = () => {
-  const [nav, showNav] = useState(false)
+   const [nav, showNav] = useState(false)
     
   return (
     <>
@@ -111,7 +111,7 @@ const Home = () => {
         {/* sidebar */}
         {/* TODO: Pass sidebar component to index */}
         {/* Pass nav bar links into sidebar */}      
-          <Global />
+          {/* <Global />
           <MenuIcon nav={nav} onClick={() => showNav(!nav)}>
             <div/>
             <div/>
@@ -132,8 +132,8 @@ const Home = () => {
               <li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
               <li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
             </ul>
-          </Menulinks>
-          
+          </Menulinks> */}
+          <Sidebar></Sidebar>
         {/* Intro */}
         <div id="intro">
           <h1>My Portfolio</h1>
